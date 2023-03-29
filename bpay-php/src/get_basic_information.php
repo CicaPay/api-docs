ea3e97f4eb4fe59f2f46769500a5855b50443455
Bpay <129297904+bpaycorp@users.noreply.github.com>
2023-03-29 16:07:43 +0200

    Add files via upload

---
+++ bpay-php/src/get_basic_information.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once('../src/BpayCurlRequest.php');
+require_once '../src/BpayAPI.php';
+
+/** Scenario: Retrieve transaction and user balance information.**/
+
+// Create a new API wrapper instance
+$Bpay = new BpayAPI;
+
+// get an transaction information
+
+// Enter your meTa data
+$id = "";
+
+// Make call to API to get info
+try {
+    $reQuest = $Bpay->getTransactionInfo($id);
+} catch (Exception $e) {
+    echo 'Error: ' . $e->getMessage();
+    exit();
+}
+
+// get an transaction status
+
+// Enter your meTa data
+$id = "";
+
+// Make call to API to get status
+try {
+    $reQuest = $Bpay->getTransactionStatus($id);
+} catch (Exception $e) {
+    echo 'Error: ' . $e->getMessage();
+    exit();
+}
+
+// get user available balance
+
+// Make call to API to get balance
+try {
+    $reQuest = $Bpay->getBalance();
+} catch (Exception $e) {
+    echo 'Error: ' . $e->getMessage();
+    exit();
+}
