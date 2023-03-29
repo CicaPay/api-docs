78c8a15dd005aaaea55c1207ddaa670e61b9a511
Bpay <129297904+bpaycorp@users.noreply.github.com>
2023-03-29 16:49:48 +0200

    Add files via upload

---
+++ bpay-js/payInPro.js
@@ -0,0 +1,52 @@
+
+// set api key, you can get it on bpay.bryocorp.com
+const apiKey = "xxxxxxxxxxxxxxxxxxxxxxxx";
+
+// set request mode
+const mode = "live"; // test or live
+
+// SEND PAY IN PRO REQUEST 
+
+var order = [
+    {
+        'name': 'Pneu',
+        'unit_price': 100,
+        'quantity': 10
+    },
+    {
+        'name': 'Par brise',
+        'unit_price': 1000,
+        'quantity': 25
+    },
+    {
+        'name': 'Volant',
+        'unit_price': 160,
+        'quantity': 29
+    }
+]
+var data = {
+    "mobile_money_number": "65000000",
+    "description": "Achat de matériels",
+    "first_name": "Cica",
+    "last_name": "Hhhhhhhh",
+    "email": "example@gmail.com",
+    "customer_company": "HOCI SARL",
+    "network": "moov_bj",
+    "order": order
+};
+
+fetch(`https://bpay.bryocorp.com/e_merchant_own/${mode}/pay_in/${apiKey}`, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        // OR "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+})
+    .then((response) => response.json())
+    .then((data) => {
+        console.log("Success:", data);
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
---
+++ bpay-js/payOut.js
@@ -0,0 +1,35 @@
+
+// set api key, you can get it on bpay.bryocorp.com
+const apiKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxx";
+
+// set request mode
+const mode = "live"; // test or live
+
+
+// SEND PAY OUT REQUEST
+
+const data =
+{
+    "mobile_money_number": 65000000,
+    "amount": 500000,
+    "first_name": "yves",
+    "last_name": "Hhhhhhh",
+    "email": "example@proton.com",
+    "network": "moov_bj"
+};
+
+fetch(`https://bpay.bryocorp.com/e_merchant_own/${mode}/pay_out/${apiKey}`, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        // OR "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+})
+    .then((response) => response.json())
+    .then((data) => {
+        console.log("Success:", data);
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
