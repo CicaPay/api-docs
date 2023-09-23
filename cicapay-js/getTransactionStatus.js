1652bef171cce43eb5a44009fb34ef6a24eb585c
CicaPay <support@cicapay.com>
2023-09-23 17:01:48 +0200

    Update endpoints

--- cicapay-js/getTransactionStatus.js
+++ cicapay-js/getTransactionStatus.js
@@ -8,7 +8,7 @@ const mode = "live"; // test or live
 
 
 // GET TRANSACTION STATUS
-fetch(`https://cicapay.com/e_merchant_own/${mode}/check_transaction_status/${apiKey}/${transaction_id}`, {
+fetch(`https://api.cicapay.com/e_merchant_own/${mode}/check_transaction_status/${apiKey}/${transaction_id}`, {
     method: "GET",
     headers: {
         "Content-Type": "application/x-www-form-urlencoded",
